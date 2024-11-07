9f4dd01d249ca2af31fa0c4af6c2cf3fa7ca8006
ahmadsultani <ahmadsultanidayanullah@gmail.com>
2024-11-08 00:35:24 +0800

    fix: footer image

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,57 +1,57 @@
 @php
-    $links = [
-        ['name' => 'Twitter', 'path' => 'icons/twitter.svg'],
-        ['name' => 'Instagram', 'path' => 'icons/instagram.svg'],
-        ['name' => 'Pinterest', 'path' => 'icons/pinterest.svg'],
-        ['name' => 'Youtube', 'path' => 'icons/youtube.svg'],
-    ];
+$links = [
+['name' => 'Twitter', 'path' => '/icons/twitter.svg'],
+['name' => 'Instagram', 'path' => '/icons/instagram.svg'],
+['name' => 'Pinterest', 'path' => '/icons/pinterest.svg'],
+['name' => 'Youtube', 'path' => '/icons/youtube.svg'],
+];
 
-    $rightNavs =
-        [
-            //1
-            [
-                [
-                    'title' => 'About Us',
-                    'links' => [
-                        ['text' => 'Our Mission', 'url' => '/'],
-                        ['text' => 'Partnership', 'url' => '/'],
-                        ['text' => 'Accountability', 'url' => '/']
-                    ]
-                ],
-                [
-                    'title' => 'Our Reach',
-                    'links' => [
-                        ['text' => 'Provinces And Region', 'url' => '/'],
+$rightNavs =
+[
+//1
+[
+[
+'title' => 'About Us',
+'links' => [
+['text' => 'Our Mission', 'url' => '/'],
+['text' => 'Partnership', 'url' => '/'],
+['text' => 'Accountability', 'url' => '/']
+]
+],
+[
+'title' => 'Our Reach',
+'links' => [
+['text' => 'Provinces And Region', 'url' => '/'],
 
-                    ]
-                ],
+]
+],
 
-            ],
-            //2
-            [
-                [
-                    'title' => 'Our Works',
-                    'links' => [
-                        ['text' => 'Disaster Response Plan', 'url' => '/'],
-                        ['text' => 'Disaster Risk Reduction', 'url' => '/'],
-                        ['text' => 'Innovation and Tech', 'url' => '/'],
-                        ['text' => 'IT System', 'url' => '/'],
-                        ['text' => 'System Management Development', 'url' => '/'],
-                    ]
-                ]
-            ],
-            //3
-            [
-                [
-                    'title' => 'Get Involved',
-                    'links' => [
-                        ['text' => 'Donate', 'url' => '/'],
-                        ['text' => 'Share The Meal App', 'url' => '/'],
-                    ]
-                ]
-            ],
+],
+//2
+[
+[
+'title' => 'Our Works',
+'links' => [
+['text' => 'Disaster Response Plan', 'url' => '/'],
+['text' => 'Disaster Risk Reduction', 'url' => '/'],
+['text' => 'Innovation and Tech', 'url' => '/'],
+['text' => 'IT System', 'url' => '/'],
+['text' => 'System Management Development', 'url' => '/'],
+]
+]
+],
+//3
+[
+[
+'title' => 'Get Involved',
+'links' => [
+['text' => 'Donate', 'url' => '/'],
+['text' => 'Share The Meal App', 'url' => '/'],
+]
+]
+],
 
-        ]
+]
 
 @endphp
 
@@ -60,7 +60,7 @@
     <main class=" max-w-[1440px] gap-6 md:gap-7 lg:gap-8 w-full flex flex-col lg:flex-row items-start justify-center">
         <section class="flex flex-col gap-6 lg:max-w-[320px]">
             <div class="text-primary text-[24px] md:text-[28px] lg:text-[32px] flex gap-3 items-center">
-                <img src="{{ asset(path: 'images/Logo.png') }}" alt="Site Logo" class="h-[30px] md:h-[36px] lg:h-[42px]">
+                <img src="{{ asset(path: '/images/Logo.png') }}" alt="Site Logo" class="h-[30px] md:h-[36px] lg:h-[42px]">
                 DMC IKATEK-UH
             </div>
             <p class="text-white text-[10px] md:text-[12px] font-[500]">
@@ -94,11 +94,11 @@
                 </div>
                 <div class="flex gap-4">
                     @foreach ($links as $link)
-                        <a>
-                            <div class="h-8 w-8 items-center flex rounded-[50%] bg-white justify-center cursor-pointer">
-                                <img src="{{$link['path']}}" />
-                            </div>
-                        </a>
+                    <a>
+                        <div class="h-8 w-8 items-center flex rounded-[50%] bg-white justify-center cursor-pointer">
+                            <img src="{{$link['path']}}" />
+                        </div>
+                    </a>
                     @endforeach
                 </div>
             </nav>
@@ -109,7 +109,7 @@
                 <div class="flex gap-4 lg:flex-1">
                     <div
                         class="bg-[rgba(220,134,48,0.20)] hidden md:inline md:sm max-w-[60px] max-h-[60px] rounded-lg p-3">
-                        <img src="icons/sms.svg" />
+                        <img src="/icons/sms.svg" />
                     </div>
                     <div class="flex text-white flex-col justify-between">
                         <p class="sm:text-[16px] md:text-[20px] lg:text-[24px] font-semibold min-w-[120px]">Enter Your
@@ -134,33 +134,33 @@
             <!-- Right Navigation Menu (mapped from $rightNavs) -->
             <div class="grid grid-cols-2 gap-4 md:grid-cols-3 w-full lg:grid-cols-3">
                 @foreach ($rightNavs as $navSection)
-                    <!-- Each $navSection contains an array of navigation groups -->
-                    <nav class="flex flex-col gap-4 md:gap-6 lg:gap-8">
-                        @foreach ($navSection as $navGroup)
-                            <!-- Title of the navigation group -->
-                            <div class="flex flex-col gap-1 md:gap-4">
-                                <div class="flex gap-2 items-center">
-                                    <div class="bg-primary h-1 w-[15px] rounded-xl"></div>
-                                    <p class="text-white text-[16px] lg:text-[18px] font-semibold">{{ $navGroup['title'] }}</p>
+                <!-- Each $navSection contains an array of navigation groups -->
+                <nav class="flex flex-col gap-4 md:gap-6 lg:gap-8">
+                    @foreach ($navSection as $navGroup)
+                    <!-- Title of the navigation group -->
+                    <div class="flex flex-col gap-1 md:gap-4">
+                        <div class="flex gap-2 items-center">
+                            <div class="bg-primary h-1 w-[15px] rounded-xl"></div>
+                            <p class="text-white text-[16px] lg:text-[18px] font-semibold">{{ $navGroup['title'] }}</p>
+                        </div>
+                        <ul class="flex flex-col gap-1">
+                            <!-- Iterate over the links in the group -->
+                            @foreach ($navGroup['links'] as $link)
+                            <div class="flex gap-1 md:gap-2 items-center text-white">
+                                <div>
+                                    <x-bladewind::icon name="chevron-right" class="!h-4 !w-3 " />
                                 </div>
-                                <ul class="flex flex-col gap-1">
-                                    <!-- Iterate over the links in the group -->
-                                    @foreach ($navGroup['links'] as $link)
-                                        <div class="flex gap-1 md:gap-2 items-center text-white">
-                                            <div>
-                                                <x-bladewind::icon name="chevron-right" class="!h-4 !w-3 " />
-                                            </div>
 
-                                            <a href="{{ $link['url'] }}"
-                                                class="text-white-dark text-[12px] lg:text-[14px] hover:text-primary transition-[200ms] hover:transition-[200ms]">{{ $link['text'] }}</a>
-                                        </div>
-                                        <li>
-                                        </li>
-                                    @endforeach
-                                </ul>
+                                <a href="{{ $link['url'] }}"
+                                    class="text-white-dark text-[12px] lg:text-[14px] hover:text-primary transition-[200ms] hover:transition-[200ms]">{{ $link['text'] }}</a>
                             </div>
-                        @endforeach
-                    </nav>
+                            <li>
+                            </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    @endforeach
+                </nav>
                 @endforeach
             </div>
 
@@ -186,11 +186,11 @@
             </div>
             <div class="flex gap-4">
                 @foreach ($links as $link)
-                    <a>
-                        <div class="h-6 w-6 items-center flex rounded-[50%] bg-white justify-center cursor-pointer">
-                            <img src="{{$link['path']}}" class="h-4 w-4" />
-                        </div>
-                    </a>
+                <a>
+                    <div class="h-6 w-6 items-center flex rounded-[50%] bg-white justify-center cursor-pointer">
+                        <img src="{{$link['path']}}" class="h-4 w-4" />
+                    </div>
+                </a>
                 @endforeach
             </div>
         </nav>
