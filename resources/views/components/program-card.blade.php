d40873422ef7b266734f178964344cd10e511a70
arctanziru <ahmadsultanidayanullah@gmail.com>
2025-01-01 09:33:01 +0800

    fix: comment error

--- resources/views/components/program-card.blade.php
+++ resources/views/components/program-card.blade.php
@@ -1,10 +1,10 @@
 @php
-    $percentage = $target > 0 ? ceil(($totalDonation / $target) * 100) : 0; // Prevent zero division error
-    $totalDonationFormatted = number_format($totalDonation, 0, ',', '.');
-    $targetFormatted = $target > 0 ? number_format($target, 0, ',', '.') : 'N/A'; // Display 'N/A' if no target
+$percentage = $target > 0 ? ceil(($totalDonation / $target) * 100) : 0; // Prevent zero division error
+$totalDonationFormatted = number_format($totalDonation, 0, ',', '.');
+$targetFormatted = $target > 0 ? number_format($target, 0, ',', '.') : 'N/A'; // Display 'N/A' if no target
 
-    $widthClass = $fullwidth ? "w-full" : "max-w-[224px]";
-    $imageCl = $fullwidth ? "w-full" : "w-[200px]";
+$widthClass = $fullwidth ? "w-full" : "max-w-[224px]";
+$imageCl = $fullwidth ? "w-full" : "w-[200px]";
 @endphp
 
 
@@ -16,10 +16,10 @@
         <p class="uppercase text-primary text-[10px] font-bold">{{ $status }}</p>
     </section>
     <section class="flex flex-col gap-2 w-full">
-        @if ($target > 0) <!-- Show progress bar only if target is greater than zero -->
-            <div class="p-[1px] w-full bg-white-dark rounded-[20px] items-center justify-start flex">
-                <div class="w-[{{ $percentage }}%] bg-primary rounded-[20px] h-[6px]"></div>
-            </div>
+        @if ($target > 0)
+        <div class="p-[1px] w-full bg-white-dark rounded-[20px] items-center justify-start flex">
+            <div class="w-[{{ $percentage }}%] bg-primary rounded-[20px] h-[6px]"></div>
+        </div>
         @endif
         <section class="grid grid-cols-2 w-full">
             <div>
@@ -33,10 +33,9 @@
     <section class="self-stretch flex-1 flex flex-col">
         <div class="flex">
             @if ($location)
-                <!-- <x-bladewind::icon name="map-pin" class="h-[14px]" /> -->
-                <p class="text-[12px] text-secondary capitalize">
-                    {{ ucwords(strtolower($location)) }}
-                </p>
+            <p class="text-[12px] text-secondary capitalize">
+                {{ ucwords(strtolower($location)) }}
+            </p>
             @else
 
             @endif
